ae5225397c502cea7f9080973a208f0efccb2d78
Nahom Tesfu <nahomtes2028@hmacademy.org>
2025-10-30 16:04:06 +0000

    fixed

--- Question Generator.py
+++ Question Generator.py
@@ -12,12 +12,13 @@ class Question(BaseModel):
 class Questions(BaseModel):
     questions: list[Question]
 model = GroqModel(
+    
     'qwen/qwen3-32b', provider=GroqProvider(api_key='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')
 )
 agent = Agent(output_type=Questions, system_prompt=(
         "Act as an expert mcq question generator based on the topic and grade level."
     ),model=model)
-print("##################################################################################################################\n Question Generator \n #####################################################################################################")
+print("##################################################################################################################\n \n Question Generator  \n \n##################################################################################################################")
 topic = input("Please Enter the topic you want questions on: ")
 grade = input("Please Enter your grade level: ")
 
